729cdbe7a7cdbe1b855afd045ab7fcead31bf429
Andrew Snodgrass <git@pointlander.info>
2018-10-25 16:27:08 -0600

    Added api example

---
+++ examples/api/basic-gateway/README.md
@@ -0,0 +1,18 @@
+# Basic Gateway
+
+## Testing
+
+Run:
+```
+go run main.go
+```
+
+Then open another terminal and run:
+```
+curl http://localhost:9096/pets/1
+```
+
+You should then see something like:
+```
+{"category":{"id":0,"name":"string"},"id":1,"name":"aspen","photoUrls":["string"],"status":"done","tags":[{"id":0,"name":"string"}]}
+```
---
+++ examples/api/basic-gateway/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"github.com/project-flogo/contrib/activity/rest"
+	trigger "github.com/project-flogo/contrib/trigger/rest"
+	"github.com/project-flogo/core/api"
+	"github.com/project-flogo/core/engine"
+	"github.com/project-flogo/microgateway"
+	microapi "github.com/project-flogo/microgateway/api"
+)
+
+func main() {
+	app := api.NewApp()
+
+	gateway := microapi.New("Pets")
+	service := gateway.NewService("PetStorePets", &rest.Activity{})
+	service.SetDescription("Get pets by ID from the petstore")
+	service.AddSetting("uri", "http://petstore.swagger.io/v2/pet/:petId")
+	service.AddSetting("method", "GET")
+	step := gateway.NewStep(service)
+	step.AddInput("pathParams", "=$.payload.pathParams")
+	response := gateway.NewResponse(false)
+	response.SetCode(200)
+	response.SetData("=$.PetStorePets.outputs.result")
+	settings, err := gateway.AddResource(app)
+	if err != nil {
+		panic(err)
+	}
+
+	trg := app.NewTrigger(&trigger.Trigger{}, &trigger.Settings{Port: 9096})
+	handler, err := trg.NewHandler(&trigger.HandlerSettings{
+		Method: "GET",
+		Path:   "/pets/:petId",
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = handler.NewAction(&microgateway.Action{}, settings)
+	if err != nil {
+		panic(err)
+	}
+
+	e, err := api.NewEngine(app)
+	if err != nil {
+		panic(err)
+	}
+	engine.RunEngine(e)
+}
