33b09b34b48435b81fa39dceff12b82dba6063bf
Andrew Snodgrass <git@pointlander.info>
2018-11-15 13:28:48 -0700

    Consolidated duplicate code

--- activity/anomaly/activity_test.go
+++ activity/anomaly/activity_test.go
@@ -12,16 +12,11 @@ import (
 	"testing"
 	"time"
 
-	"github.com/project-flogo/contrib/activity/rest"
-	trigger "github.com/project-flogo/contrib/trigger/rest"
 	"github.com/project-flogo/core/activity"
-	"github.com/project-flogo/core/api"
 	"github.com/project-flogo/core/data"
 	"github.com/project-flogo/core/data/mapper"
 	"github.com/project-flogo/core/data/metadata"
 	logger "github.com/project-flogo/core/support/log"
-	"github.com/project-flogo/microgateway"
-	microapi "github.com/project-flogo/microgateway/api"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -267,49 +262,7 @@ func TestIntegration(t *testing.T) {
 	}
 	defer s.Shutdown(context.Background())
 
-	app := api.NewApp()
-
-	gateway := microapi.New("Test")
-	serviceAnomaly := gateway.NewService("Anomaly", &Activity{})
-	serviceAnomaly.SetDescription("Look for anomalies")
-	serviceAnomaly.AddSetting("depth", 3)
-
-	serviceUpdate := gateway.NewService("Update", &rest.Activity{})
-	serviceUpdate.SetDescription("Make calls to test")
-	serviceUpdate.AddSetting("uri", "http://localhost:1234/test")
-	serviceUpdate.AddSetting("method", "PUT")
-
-	step := gateway.NewStep(serviceAnomaly)
-	step.AddInput("payload", "=$.payload.content")
-	step = gateway.NewStep(serviceUpdate)
-	step.SetIf("($.Anomaly.outputs.count < 100) || ($.Anomaly.outputs.complexity < 3)")
-	step.AddInput("content", "=$.payload.content")
-
-	response := gateway.NewResponse(false)
-	response.SetIf("($.Anomaly.outputs.count < 100) || ($.Anomaly.outputs.complexity < 3)")
-	response.SetCode(200)
-	response.SetData("=$.Update.outputs.data")
-	response = gateway.NewResponse(true)
-	response.SetCode(403)
-	response.SetData(map[string]interface{}{
-		"error":      "anomaly!",
-		"complexity": "=$.Anomaly.outputs.complexity",
-	})
-
-	settings, err := gateway.AddResource(app)
-	assert.Nil(t, err)
-
-	trg := app.NewTrigger(&trigger.Trigger{}, &trigger.Settings{Port: 9096})
-	handler, err := trg.NewHandler(&trigger.HandlerSettings{
-		Method: "PUT",
-		Path:   "/test",
-	})
-	assert.Nil(t, err)
-
-	_, err = handler.NewAction(&microgateway.Action{}, settings)
-	assert.Nil(t, err)
-
-	e, err := api.NewEngine(app)
+	e, err := Example()
 	assert.Nil(t, err)
 	e.Start()
 	defer e.Stop()
