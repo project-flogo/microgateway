4713f563b8774650ebda8c006247f85e48df6dc7
Akshay Gadikar <akshayug@gmail.com>
2019-03-04 15:12:42 -0800

    Moved pattern.go to root directory

--- internal/pattern/assets.go
+++ internal/pattern/assets.go
@@ -161,7 +161,7 @@ func AssetNames() []string {
 
 // _bindata is a table, holding each asset generator, mapped to its name.
 var _bindata = map[string]func() (*asset, error){
-	"DefaultHttpPattern.json": defaulthttppatternJson,
+	"DefaultHttpPattern.json":    defaulthttppatternJson,
 	"DefaultChannelPattern.json": defaultchannelpatternJson,
 }
 
@@ -204,9 +204,10 @@ type bintree struct {
 	Func     func() (*asset, error)
 	Children map[string]*bintree
 }
+
 var _bintree = &bintree{nil, map[string]*bintree{
 	"DefaultChannelPattern.json": &bintree{defaultchannelpatternJson, map[string]*bintree{}},
-	"DefaultHttpPattern.json": &bintree{defaulthttppatternJson, map[string]*bintree{}},
+	"DefaultHttpPattern.json":    &bintree{defaulthttppatternJson, map[string]*bintree{}},
 }}
 
 // RestoreAsset restores an asset under the given directory
@@ -255,4 +256,3 @@ func _filePath(dir, name string) string {
 	cannonicalName := strings.Replace(name, "\\", "/", -1)
 	return filepath.Join(append([]string{dir}, strings.Split(cannonicalName, "/")...)...)
 }
-
--- internal/pattern/generate.go
+++ internal/pattern/generate.go
@@ -1,4 +1,3 @@
 //go:generate go-bindata -pkg pattern -o assets.go DefaultHttpPattern.json DefaultChannelPattern.json
 
 package pattern
-
